8264a96df81a073005f1275a27e632cff1b6443d
AyazAhmedTurk5 <ayaz.ahmed@emumba.com>
2025-02-12 20:40:32 +0500

    vercel config added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "frontend/dist", // Ensure this matches the distDir in vercel.json
+    chunkSizeWarningLimit: 1000, // Optional, for the chunk size warning
+  },
+});
