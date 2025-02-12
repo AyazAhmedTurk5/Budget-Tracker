bcadf1a686fd2cbb551eb750c783d4292ad99cd7
AyazAhmedTurk5 <ayaz.ahmed@emumba.com>
2025-02-12 20:54:33 +0500

    Changed the vercel configuration and json files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "frontend/dist", // Ensure this matches the distDir in vercel.json
-    // chunkSizeWarningLimit: 1000, // Optional, for the chunk size warning
-  },
 });
